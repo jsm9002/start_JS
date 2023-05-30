82d24bdf1d98b4dda8269bad89b949fe20c01379
jsm9002 <jsm9002@naver.com>
2023-05-30 18:19:47 +0900

    first commit

---
+++ 4_Nameing_Convention.js
@@ -0,0 +1,37 @@
+/**
+ * Naming Conventions
+ * 
+ * 1) 일반적으로 영어(라틴문자)를 사용하여 문자와 숫자를 모두 사용 할 수 있다.
+ * 2) 특수기호는 언더스코어와 달러를 사용 할 수 있다.
+ * 3) 숫자로 이름을 시작 할 수 없다.
+ * 1Name, 2Hello XXXXX
+ * 4) 키워드는 변수명으로 사용 할 수 없다.
+ * var const = 'var'; XXXXX
+ * 
+*/
+
+let codefactory = '코드팩토리';
+var $ive = '아이브';
+const _yujin='안유진';
+
+console.log(codefactory);
+console.log($ive);
+console.log(_yujin);
+
+//let 1name = 'no';
+//let const = 'attention';
+
+/**
+ * Nameing Conventions
+ * 
+ * 1) Camel Case -> 대부분의 언어에서 많이 사용 첫글자는 소문자 뒤에구분자 첫글자부터 대문자
+ * 
+ * 2) snake Case -> '_' 로 이름을 짓기 파이썬같은 언어에서 사용
+ * 
+ * 3) PascalCase -> c# 마이크로소프트 계열의 언어에서 사용
+ */
+
+//안유진
+const yuJin = '유진';
+console.log(yuJin);
+
